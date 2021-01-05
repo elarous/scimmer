8a313aa61da1a2936fd0122101fc5cc8c4aa750b
Oussama El Arbaoui <elarbaouioussama@gmail.com>
2021-01-05 23:47:10 +0100

    CU-5tr3ma - Initialize the front-end + add components library

--- src/cljs/scimmer/core.cljs
+++ src/cljs/scimmer/core.cljs
@@ -11,25 +11,26 @@
     [scimmer.events]
     [reitit.core :as reitit]
     [reitit.frontend.easy :as rfe]
+    ["grommet" :refer [Grommet Button]]
     [clojure.string :as string])
   (:import goog.History))
 
 (defn nav-link [uri title page]
   [:a.navbar-item
-   {:href   uri
+   {:href  uri
     :class (when (= page @(rf/subscribe [:common/page])) :is-active)}
    title])
 
-(defn navbar [] 
+(defn navbar []
   (r/with-let [expanded? (r/atom false)]
               [:nav.navbar.is-info>div.container
                [:div.navbar-brand
                 [:a.navbar-item {:href "/" :style {:font-weight :bold}} "scimmer"]
                 [:span.navbar-burger.burger
                  {:data-target :nav-menu
-                  :on-click #(swap! expanded? not)
-                  :class (when @expanded? :is-active)}
-                 [:span][:span][:span]]]
+                  :on-click    #(swap! expanded? not)
+                  :class       (when @expanded? :is-active)}
+                 [:span] [:span] [:span]]]
                [:div#nav-menu.navbar-menu
                 {:class (when @expanded? :is-active)}
                 [:div.navbar-start
@@ -42,12 +43,15 @@
 
 (defn home-page []
   [:section.section>div.container>div.content
-   (when-let [docs @(rf/subscribe [:docs])]
-     [:div {:dangerouslySetInnerHTML {:__html (md->html docs)}}])])
+   [:> Button {:primary  true
+               :label    "Grommet button"
+               :on-click #(js/alert "button clicked")}]])
+
 
 (defn page []
   (if-let [page @(rf/subscribe [:common/page])]
     [:div
+     [:h1]
      [navbar]
      [page]]))
 
