461da9c256f3aec46307eba9fad87ab5d37fd194
Oussama El Arbaoui <elarbaouioussama@gmail.com>
2021-01-24 22:56:06 +0100

    CU-cz3th3 - wrap interceptor in function and filter through the provided function as argument

--- src/cljs/scimmer/mapping/events.cljs
+++ src/cljs/scimmer/mapping/events.cljs
@@ -6,7 +6,10 @@
     [reitit.frontend.controllers :as rfc]
     [scimmer.app-db :as app-db]))
 
-(def single-attr-interceptor
+(defn single-attr-filter [[_name _props schema]]
+  (contains? #{:vector :map} (:type schema)))
+
+(defn attr-interceptor [filtering-fn]
   (rf/->interceptor
     :id :single-attr-interceptor
     :before (fn [context]
@@ -15,23 +18,21 @@
                     (-> original-db
                         (get-in [:mapping :children])
                         (as-> all-attrs
-                              (filterv (fn [[_name _props schema]]
-                                         (not (contains? #{:vector :map} (:type schema)))) all-attrs)))]
+                              (filterv (complement filtering-fn) all-attrs)))]
                 (-> context
                     (assoc :temp-db original-db)
                     (rf/assoc-coeffect :db single-attrs))))
     :after (fn [context]
              (let [original-db (:temp-db context)
                    db-without-old-attrs (update-in original-db [:mapping :children]
-                                                   #(filterv (fn [[_name _props schema]]
-                                                               (contains? #{:vector :map} (:type schema))) %))
+                                                   #(filterv filtering-fn %))
                    new-attrs-vec (rf/get-effect context :db ::not-found)
                    new-db (update-in db-without-old-attrs [:mapping :children] concat new-attrs-vec)]
                (rf/assoc-effect context :db new-db)))))
 
 (rf/reg-event-db
   :mapping/>update-single-attr
-  [single-attr-interceptor]
+  [(attr-interceptor single-attr-filter)]
   (fn [single-attrs [_ {:keys [name entity mapping]}]]
     (let [target-idx (->> single-attrs
                           (map-indexed (fn [idx itm] [idx itm]))
