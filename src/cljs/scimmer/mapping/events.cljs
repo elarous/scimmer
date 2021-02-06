cb665a48bb9a51931e5b0fad712b0c20f996b550
Oussama El Arbaoui <elarbaouioussama@gmail.com>
2021-02-06 01:07:47 +0100

    CU-dpwcbb - Include extension results into Schema in the front-end

--- src/cljs/scimmer/mapping/events.cljs
+++ src/cljs/scimmer/mapping/events.cljs
@@ -14,18 +14,3 @@
           resource (:resource db)]
       (assoc db :entities (build-resource schema resource [] {} {})))))
 
-;; TODO: gen keys for array items too and probably i split this function in to type specific functions (for objects and arrays)
-(rf/reg-event-db
-  :mapping/>gen-keys
-  [(rf/path :mapping :children)]
-  (fn [mapping _]
-    (map (fn [item]
-           (def item item)
-           (if (= (-> item (nth 2) :type) :map)
-             ;; If the attribute is an object, add keys to the sub-attrs also
-             (-> item
-                 (update-in [2 :children]
-                            (fn [sa] (map #(with-meta % {:id (random-uuid)}) sa)))
-                 (with-meta {:id (random-uuid)}))
-             (with-meta item {:id (random-uuid)})))
-         mapping)))
